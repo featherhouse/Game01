edcbfba821340d3b0c8898b5bcf2835a07f24e18
househe <50344868+househe@users.noreply.github.com>
2022-08-18 14:25:50 -0500

    added index.html and script file

---
+++ game.js
@@ -0,0 +1,41 @@
+var points = 0;
+var pointsReset = false; 
+
+//write your code here
+for (let i = 0; i < 100; i++) {
+    
+                        do{    
+
+if(points === 125) {
+     points=22;
+}  
+
+                        }
+                        while(points > 291);
+
+
+    
+ if(i % 2 === 0)    {
+     
+                      
+     
+   
+points=points+5;
+}
+
+
+
+
+   
+
+
+if(i % 2 !== 0)    {
+     
+   points=points+3;
+   
+ 
+  
+}
+
+console.log("Turns: " + i + " Score: " + points );
+}
